bdc44fde30c2381c7caa3376589f8c2f9662257c
Daniel González Lopes <danielgonzalezlopes@gmail.com>
2020-10-22 11:48:39 +0200

    Add tests for headers
    Signed-off-by: Daniel González Lopes <danielgonzalezlopes@gmail.com>

--- faultinjection_test.go
+++ faultinjection_test.go
@@ -8,6 +8,11 @@ import (
 	"time"
 )
 
+type Header struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
 func TestServeHTTPp(t *testing.T) {
 	tests := []struct {
 		desc                    string
@@ -96,6 +101,101 @@ func TestServeHTTPp(t *testing.T) {
 			recorder := httptest.NewRecorder()
 
 			req := httptest.NewRequest(http.MethodGet, "http://localhost", nil)
+
+			handler.ServeHTTP(recorder, req)
+
+			elapsed := time.Since(start)
+
+			if elapsed.Seconds() > test.expDelayDurationSeconds {
+				t.Errorf("got unexpected delay: enlapsed %f expected %f", elapsed.Seconds(), test.expDelayDurationSeconds)
+			}
+
+			if recorder.Result().StatusCode != test.expStatusCode {
+				t.Errorf("got status code %d, want %d", recorder.Code, test.expStatusCode)
+			}
+		})
+	}
+}
+
+func TestHeaders(t *testing.T) {
+	tests := []struct {
+		desc                    string
+		headers                 []Header
+		delay                   bool
+		abort                   bool
+		expDelayDurationSeconds float64
+		expStatusCode           int
+	}{
+		{
+			desc:                    "delay is disabled and header is set",
+			headers:                 []Header{{Key: "x-traefik-fault-delay-request", Value: "5000"}, {Key: "x-traefik-fault-delay-request-percentage", Value: "100"}},
+			delay:                   false,
+			abort:                   false,
+			expDelayDurationSeconds: 0.5,
+			expStatusCode:           http.StatusOK,
+		}, {
+			desc:                    "delay is enabled and header is set",
+			headers:                 []Header{{Key: "x-traefik-fault-delay-request", Value: "5000"}, {Key: "x-traefik-fault-delay-request-percentage", Value: "100"}},
+			delay:                   true,
+			abort:                   false,
+			expDelayDurationSeconds: 5.5,
+			expStatusCode:           http.StatusOK,
+		}, {
+			desc:                    "delay is enabled and header is incorrect",
+			headers:                 []Header{{Key: "x-traefik-fault-delay-request", Value: "5000"}, {Key: "x-traefik-fault-delay-request-percentage", Value: "notint"}},
+			delay:                   true,
+			abort:                   false,
+			expDelayDurationSeconds: 0.5,
+			expStatusCode:           http.StatusOK,
+		}, {
+			desc:                    "abort is disabled and header is set",
+			headers:                 []Header{{Key: "x-traefik-fault-abort-request", Value: "400"}, {Key: "x-traefik-fault-abort-percentage", Value: "100"}},
+			delay:                   false,
+			abort:                   false,
+			expDelayDurationSeconds: 0.5,
+			expStatusCode:           http.StatusOK,
+		}, {
+			desc:                    "abort is enabled and header is set",
+			headers:                 []Header{{Key: "x-traefik-fault-abort-request", Value: "400"}, {Key: "x-traefik-fault-abort-percentage", Value: "100"}},
+			delay:                   false,
+			abort:                   true,
+			expDelayDurationSeconds: 0.5,
+			expStatusCode:           http.StatusBadRequest,
+		}, {
+			desc:                    "abort is enabled and header is incorrect",
+			headers:                 []Header{{Key: "x-traefik-fault-abort-request", Value: "dd"}, {Key: "x-traefik-fault-abort-percentage", Value: "ee"}},
+			delay:                   false,
+			abort:                   true,
+			expDelayDurationSeconds: 0.5,
+			expStatusCode:           http.StatusBadRequest,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			cfg := &Config{
+				Delay:           test.delay,
+				Abort:           test.abort,
+				AbortCode:       400,
+				AbortPercentage: 100,
+			}
+
+			start := time.Now()
+
+			next := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {})
+
+			handler, err := New(context.Background(), next, cfg, "fault")
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			recorder := httptest.NewRecorder()
+
+			req := httptest.NewRequest(http.MethodGet, "http://localhost", nil)
+
+			for _, header := range test.headers {
+				req.Header.Set(header.Key, header.Value)
+			}
 
 			handler.ServeHTTP(recorder, req)
 
